0a9c4973b79cf4a39cd3b1e1fc76a43f84b76447
bibo <bibaso@gmail.com>
2024-12-11 23:18:28 +0100

    Day 11 test adjustment

--- day11/test.js
+++ day11/test.js
@@ -1,14 +1,8 @@
 const expect = require('expect.js');
 const part1 = require('./part1');
-const part2 = require('./part2');
 
 describe('Day 11: Part 1', () => {
-  it('Should', () => {
+  it('Should count the stones after 25 blinkks', () => {
     expect(part1(`125 17`)).to.equal(55312);
-  });
-});
-describe('Day 11: Part 2', () => {
-  it('Should', () => {
-    expect(part2(`125 17`)).to.equal(55312);
   });
 });
