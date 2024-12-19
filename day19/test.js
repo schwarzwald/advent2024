1a80d7b0c234879a8bc1e7c73d86ea01cdc2b84b
bibo <bibaso@gmail.com>
2024-12-19 22:45:12 +0100

    Day 19 test adjustment

--- day19/test.js
+++ day19/test.js
@@ -3,7 +3,7 @@ const part1 = require('./part1');
 const part2 = require('./part2');
 
 describe('Day 19: Part 1', () => {
-  it('Should', () => {
+  it('Should count how many designs are possible', () => {
     expect(part1(`r, wr, b, g, bwu, rb, gb, br
 
 brwrr
@@ -17,7 +17,7 @@ bbrgwb`)).to.equal(6);
   });
 });
 describe('Day 19: Part 2', () => {
-  it('Should', () => {
+  it('Should count the number of ways how the designs can be achieved', () => {
     expect(part2(`r, wr, b, g, bwu, rb, gb, br
 
 brwrr
